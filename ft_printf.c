6baeaa7d3b957f4f1c4612779e51d0542ed28941
allarkpet <fishygayming@gmail.com>
2025-01-06 19:25:46 +0100

    {update} ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int ft_printf_format(va_list ptr, int i, const char* str)
+{
+    int return_value = 2;
+    if (str[i+1] == 'd')
+    {
+        write(1,va_arg(ptr), sizeof(int));
+        return_value = 2;
+    }
+    return (return_value);
+}
+
 int ft_printf (const char* str, ...)
 {
     va_list ptr;
@@ -11,18 +22,18 @@ int ft_printf (const char* str, ...)
     i = 0;
     while (str[i] != '\0')
     {
-       /* if (str[i] == '%')
-            //detection % je devrais aussi determiner la taille?
+        if (str[i] == '%')
+        {
+            i += ft_printf_format(ptr, i, str);
+        }
         else
-        {*/
+        {
             write(1,&str[i], 1);
             i++;
-        //}
+        }
     }
-}
-
 int main (void)
 {
-    ft_printf("ceci est un test j'adore le caca mhhh 42 oui");
+    ft_printf("ceci est un test j'adore le caca mhhh 42 %d oui", 150);
     return 0;
 }
