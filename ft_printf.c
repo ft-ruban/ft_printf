844a1f67b22865a4ef120f1c2323e2f15cbd89a0
allarkpet <fishygayming@gmail.com>
2025-01-10 12:34:40 +0100

    {fix} fixed minor issues to ft_printf.c {update} I worked on the return value, just need to do the hex stuff next

--- ft_printf.c
+++ ft_printf.c
@@ -1,45 +1,55 @@
 #include "ft_printf.h"
 #include <stdio.h>
 #include <stdint.h>
-int ft_printf_num(va_list ptr, int i, const char* str)
+int ft_printf_num(va_list ptr, int i, const char* str, int *ptr_return_value)
 {
     int value; //is it supposed to be a long?
-    int return_value;
     unsigned int uvalue;
+    int len;
+    char *str_value;
 
     value = va_arg(ptr,int);
     if (str[i+1] == 'u')
     {
         uvalue = (unsigned int) value;
-        write(1,ft_itoa (uvalue), ft_strlen(ft_itoa(uvalue)));
+        str_value = ft_utoa (uvalue);
+        len = ft_strlen(str_value);
+        write (1, str_value, len);
     }
-    if (str[i+1] == 'd' || str[i+1] == 'i') //a fix
+    else if (str[i+1] == 'd' || str[i+1] == 'i') //a fix
     {
-        write(1,ft_itoa(value), ft_strlen(ft_itoa(value)));
-        return_value = 2;
+        str_value = ft_itoa(value);
+        len = ft_strlen(str_value);
+        write (1, str_value, len);
     }
-    return (return_value);
+    *ptr_return_value += len;
+    free(str_value);
+    return (2);
 }
 
-int ft_printf_char(va_list ptr, int i, const char* str)
+int ft_printf_char(va_list ptr, int i, const char* str, int *ptr_return_value)
 {
     char value;
     char* string_temp;
-    
+    int  len;
+   
+    len = 1;
     if (str[i+1] == 'c')
     {
         value = va_arg(ptr, int);
-        write (1, &value, 1);
+        write (1, &value, len);
     }
     else if (str[i+1] == '%')
-    {
-        write (1, "%", 1);
-    }
+        write (1, "%", len);
     else if (str[i+1] == 's')
     {
         string_temp = va_arg(ptr,char *);
-        write (1, string_temp, ft_strlen((string_temp)));
+        if (!string_temp)
+            string_temp = "(null)";
+        len = ft_strlen(string_temp);
+        write (1, string_temp, len);
     }
+    *ptr_return_value += len;
     return (2);
 }
 char*    ft_itoa_hex(unsigned long nbr, int is_up)
@@ -71,7 +81,7 @@ char*    ft_itoa_hex(unsigned long nbr, int is_up)
     return (result);
 }
 
-int ft_print_hex(va_list ptr, int i, const char* str)
+int ft_print_hex(va_list ptr, int i, const char* str /*int *ptr_return_value*/)
 {
     void *string_temp;
     char *test;
@@ -101,22 +111,27 @@ int ft_print_hex(va_list ptr, int i, const char* str)
 int ft_printf (const char *str, ...)
 {
     va_list ptr;
-    va_start(ptr, str);
     int i;
-    //int return_value;
-    //int* ptr_return_value;
+    int return_value;
+    int* ptr_return_value;
 
-    //return_value = 0;
+    va_start (ptr, str);
+    return_value = 0;
+    ptr_return_value = &return_value;
     i = 0;
     while (str[i] != '\0')
     {
             if  (str[i] == '%' && (str[i+1] == 'u' || str[i+1] == 'd' || str[i+1] == 'i'))
-                i += ft_printf_num(ptr, i, str);
+                i += ft_printf_num(ptr, i, str, ptr_return_value);
             else if (str[i] == '%' && (str[i+1] == 'c' || str[i+1] == 's' || str[i+1] == '%'))
-                i += ft_printf_char(ptr, i, str);
-            else if (str[i] == '%' && (str[i+1] == 'x' || str[i+1] == 'X' || str[i+1] == 'p'))                        i += ft_print_hex(ptr, i, str);
+                i += ft_printf_char(ptr, i, str, ptr_return_value);
+            else if (str[i] == '%' && (str[i+1] == 'x' || str[i+1] == 'X' || str[i+1] == 'p'))
+                i += ft_print_hex(ptr, i, str/*, ptr_return_value*/);
             else
-            write(1,&str[i++], 1);
+            {
+                write(1,&str[i++], 1);
+                return_value += 1;
+            }
     }
-    return 1;
+    return (return_value);
 }
--- main.c
+++ main.c
@@ -15,9 +15,14 @@ int main (void)
     char hamburger[10] = "Hamburger";
     char potato [7] = "Potato";
     char yeah [7] = "Yeah";
+    int len;
+    int lenorigin;
     //unsigned int num = 20;
-    ft_printf("ceci est un test \n d = %d \n i = %i \n u = %u \n c = %c \n %% = %% \n s = %s\n p = %p\n x = %x \n X = %X\n",d, i, u, c, s,ptr,x,X);
-    printf("ceci est un test \n d = %d \n i = %i \n u = %u \n c = %c \n %% = %% \n s = %s\n p = %p\n x = %x \n X = %X\n",d, i, u, c, s,ptr,x,X);
+    len = ft_printf("ceci est un test \n d = %d \n i = %i \n u = %u \n c = %c \n %% = %% \n s = %s\n p = %p\n x = %x \n X = %X\n",d, i, u, c, s,ptr,x,X);
+    lenorigin = printf("ceci est un test \n d = %d \n i = %i \n u = %u \n c = %c \n %% = %% \n s = %s\n p = %p\n x = %x \n X = %X\n",d, i, u, c, s,ptr,x,X);
+    ft_printf("len : %d\n", len);
+    ft_printf("lenorigin : %d\n", lenorigin);
+
     ft_printf("I am eating a %s and %s everyday\n potato is good %s vegetable %s\n %s %s %s \n %s every night \n healthy, hell see, hell sea, say Hell %s %s \n By the way, do you like baseball? Yes, I am loving it! so we go to world Baseball Lunatic",hamburger, potato, yeah, yeah, eating, eating, eating, eating, yeah, yeah);
     return 0;
 }
